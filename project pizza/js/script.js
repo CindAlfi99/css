e57590d007fd6732b0db2d7b751fff4fb6794ab6
CindAlfi99 <acindi38@gmail.com>
2021-01-24 18:20:12 +0700

    update

--- project pizza/js/script.js
+++ project pizza/js/script.js
@@ -1,9 +1,36 @@
-$.getJSON('pizza.json', function(result) {
-    let menu = result.menu;
+function tampil() {
+    $.getJSON('pizza.json', function(result) {
+        let menu = result.menu;
 
-    $.each(menu, function(index, element) {
+        $.each(menu, function(index, element) {
 
-        $('#daftar-menu').append('<div class="col col-md-4"><div class="card mb-3"><img src="img/menu/' + element.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + element.nama + '</h5><p class="card-text">' + element.deskripsi + '</p><h5 class = "card-title">' + 'Rp.' + element.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+            $('#daftar-menu').append('<div class="col col-md-4"><div class="card mb-3"><img src="./img/menu/' + element.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + element.nama + '</h5><p class="card-text">' + element.deskripsi + '</p><h5 class = "card-title">' + 'Rp.' + element.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
 
+        });
     });
-});
+}
+tampil();
+
+
+
+$('.nav-link').on('click', function() {
+    $('.nav-link').removeClass('active');
+    // $(this).addClass('active');
+    let kategori = $(this).html();
+    $('h1').html(kategori);
+    if (kategori == "All Menu") {
+        tampil();
+        return;
+    }
+    // console.log(kategori);
+    $.getJSON('pizza.json', function(data) {
+        let menu = data.menu;
+        let content = '';
+        $.each(menu, function(index, element) {
+            if (element.kategori == kategori.toLowerCase()) {
+                content += '<div class="col col-md-4"><div class="card mb-3"><img src="./img/menu/' + element.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + element.nama + '</h5><p class="card-text">' + element.deskripsi + '</p><h5 class = "card-title">' + 'Rp.' + element.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
+            }
+        })
+        $('#daftar-menu').html(content);
+    })
+})
